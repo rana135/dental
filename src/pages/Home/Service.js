adc2bbba9a9b2312eb896e83ad3f50ad1a62e45e
rana135 <rana286090@gmail.com>
2022-07-16 14:38:20 +0600

    responsive

--- src/pages/Home/Service.js
+++ src/pages/Home/Service.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Service = ({ service }) => {
     return (
-        <div className="card h-auto w-72 ml-8 mr-8 shadow-xl" style={{ backgroundColor: "#e6e6e6" }} data-aos="zoom-in-up" data-aos-duration="3000">
+        <div className="card h-auto lg:w-72 ml-8 mr-8 shadow-xl" style={{ backgroundColor: "#e6e6e6" }} data-aos="zoom-in-up" data-aos-duration="3000">
             <figure className="px-10 pt-10">
                 <img  src={service.img} alt="Shoes" className="rounded-xl w-16" />
             </figure>
